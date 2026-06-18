81e8437723f7dd1248360940b36eda5af30fb3ea
dhm-blue-phoenix <blue.dhm.dev@gmail.com>
2026-06-18 14:59:41 +0200

    fiexed: getProfile Data

--- src/app/shared/interfaces/environment.ts
+++ src/app/shared/interfaces/environment.ts
@@ -1,7 +1,14 @@
 export interface Environment {
   supabaseUrl: string;
   supabaseKey: string;
+
+  debug_user_name: string;
   debug_user_email: string;
   debug_user_password: string;
+
+  debug_user2_name: string;
+  debug_user2_email: string;
+  debug_user2_password: string;
+
   debug_logs: boolean;
 }
--- src/app/shared/services/db/db.ts
+++ src/app/shared/services/db/db.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/environment';
 
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +9,7 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 export class DataBase {
   private readonly debug_logs: boolean = environment.debug_logs;
   private readonly supabase: SupabaseClient = createClient(environment.supabaseUrl, environment.supabaseKey);
-
+  private currentUserId: string = '';
 
   constructor() {
     if (this.debug_logs) {
@@ -19,56 +19,79 @@ export class DataBase {
 
 
   private async debuging(): Promise<void> {
-    console.log('environment', environment);
-    await this.signUpNewUser(environment.debug_user_email, environment.debug_user_password);
-    //await this.signInWithEmail(environment.debug_user_email, environment.debug_user_password);
+    //console.log('environment', environment);
+    //await this.signUpNewUser(environment.debug_user_email, environment.debug_user_password, environment.debug_user_name);
+    //await this.signUpNewUser(environment.debug_user2_email, environment.debug_user2_password, environment.debug_user2_name);
+    await this.signInWithEmail(environment.debug_user_email, environment.debug_user_password);
+    //await this.signOut();
 
-    await this.getProfile();
-
-    //this.signOut();
+    await this.getProfile(this.currentUserId);
   }
 
 
-  public async signUpNewUser(user_email: string, user_password: string): Promise<void> {
+  /*
+  * Legt einen neuen Benutzer an
+  */
+  public async signUpNewUser(user_email: string, user_password: string, user_name: string): Promise<void> {
     const { data, error } = await this.supabase.auth.signUp({
       email: user_email,
-      password: user_password
+      password: user_password,
+      options: {
+        data: { name: user_name }
+      }
     });
-    if (this.debug_logs && error) {
-      console.error('signUpNewUser_error', error);
+    if (this.debug_logs) {
+      if (error) console.error('signUpNewUser_error', error);
+      console.log('signUpNewUser_data', data);
     }
-    console.log('signUpNewUser_data', data);
   }
 
 
+  /*
+  * Meldet den user an
+  */
   public async signInWithEmail(user_email: string, user_password: string): Promise<void> {
     const { data, error } = await this.supabase.auth.signInWithPassword({
       email: user_email,
       password: user_password,
     });
-    if (this.debug_logs && error) {
-      console.error('signInWithEmail_error', error);
+    if (this.debug_logs) {
+      if (error) console.error('signInWithEmail_error', error);
+      console.log('signInWithEmail_data', data);
     }
-    console.log('signInWithEmail_data', data);
+    const userData: User | null = data['user'];
+    if (userData) {
+      this.currentUserId = userData['id'];
+    }
   }
 
 
+  /*
+  * Schiekt den user eine email um sein pw zu rezeten
+  */
   public async resetPasswordForEmail() { }
 
 
+  /*
+  * Hirmit wirt der user ausgeloggt
+  */
   public async signOut(): Promise<void> {
     const { error } = await this.supabase.auth.signOut();
+    if (this.debug_logs && error) {
+      console.error('signOut_error', error);
+    }
   }
 
 
-  public async getProfile(): Promise<void> {
+  public async getProfile(userId: string): Promise<void> {
     let { data: profiles, error } = await this.supabase
       .from('profiles')
-      .select('*');
+      .select('*')
+      .eq('id', userId);
 
-    if (this.debug_logs && error) {
-      console.error('getProfile_error', error);
+    if (this.debug_logs) {
+      if (error) console.error('getProfile_error', error);
+      console.log('profiles', profiles);
     }
-    console.log('profiles', profiles);
   }
 }
