876120573617b4f34ac304786b9c2881fa78f85c
Maik G <maik.g@outlook.com>
2026-06-21 19:09:34 +0200

    Add: Initial commit for main-component and add routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './shared/components/main-component/main-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+
+    { path: '**', redirectTo: '' },
+];
