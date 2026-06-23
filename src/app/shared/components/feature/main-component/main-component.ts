07562a1418ac9aeb439f19058568cd95db881aeb
Maik G <maik.g@outlook.com>
2026-06-23 10:02:57 +0200

    feat: add channels component with dynamic channel rendering and styles

--- src/app/shared/components/feature/main-component/main-component.ts
+++ src/app/shared/components/feature/main-component/main-component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 import { Workspace } from '../../ui/workspace/workspace'
+import { Channels } from '../../ui/channels/channels'
 import { Profile } from '../../../interfaces/profile';
+import { Interface } from 'readline';
 
+interface ChannelInterface{
+    id: string;
+    name: string;
+    description: string;
+    created_at: string;
+    edited_at: string;
+    members: Member[];
+    messages: Message[];
+}
 
+interface Member{
+    user_id: string;
+    role: 'admin' | 'member';
+}
+interface Message{
+    id: string;
+    sender_id: string;
+    content: string;
+    timestamp: string;
+    edited_at: string;
+    // reactions: Reaction[];
+    threads_id: string;
+}
 @Component({
   selector: 'app-main-component',
-  imports: [Workspace,],
+  imports: [Workspace, Channels],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
@@ -16,7 +40,12 @@ user: Profile[] = [
 {  id: '2', email: 'EliasNeumann@web.de', name:"Fred Neumann", created_at: '01.01.1993', status: 'offline', avatar_url: 'assets/svg/avatar/avatar_small/2.svg'},
 {  id: '3', email: 'EliasNeumann@web.de', name:"Peter Lustig", created_at: '01.01.1993', status: 'offline', avatar_url: 'assets/svg/avatar/avatar_small/3.svg'},
 {  id: '4', email: 'EliasNeumann@web.de', name:"Anna Hansen", created_at: '01.01.1993', status: 'online', avatar_url: 'assets/svg/avatar/avatar_small/4.svg'},
+  ]
+
+channel: ChannelInterface[] = [
+  {  id: '1', name: 'Entwicklerteam', description:"", created_at: '01.01.1993', edited_at: '01.01.1994', members: [], messages: []},
+  {  id: '2', name: 'Test', description:"", created_at: '01.01.1993', edited_at: '01.01.1994', members: [], messages: []},
 
 ]
-
 }
+export type Channel = ChannelInterface;
