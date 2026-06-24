09c062539553cd062eaf3960bb5f136cebbcb563
Maik G <maik.g@outlook.com>
2026-06-24 16:57:38 +0200

    feat: update user profile interface and implement user selection functionality in workspace component

--- src/app/shared/interfaces/profile.ts
+++ src/app/shared/interfaces/profile.ts
@@ -1,5 +1,5 @@
 export interface Profile {
-  id?: string;
+  id: string;
   email: string;
   name: string;
   created_at: string;
--- src/app/shared/models/profile.ts
+++ src/app/shared/models/profile.ts
@@ -1,6 +1,7 @@
 import { Profile } from '../interfaces/profile';
 
 export const emptyProfile: Profile = {
+  id: '',
   email: 'n/a',
   name: 'n/a',
   created_at: 'n/a',
