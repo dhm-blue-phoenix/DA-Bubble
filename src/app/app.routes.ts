b0161eb5b5acf66fc0d8703b2ea9647c601fab49
Maik G <maik.g@outlook.com>
2026-06-29 17:13:33 +0200

    feat: add login component with HTML, CSS, and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/feature/layout-component';
+import { LoginComponent } from './shared/components/feature/login-component/login-component';
 
 export const routes: Routes = [
     { path: '', component: LayoutComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' },
 ];
