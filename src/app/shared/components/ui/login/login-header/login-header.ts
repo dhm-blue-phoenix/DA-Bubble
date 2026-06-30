642ce5cd3761953312d38c5e6f41b4a71f30c92e
Maik G <maik.g@outlook.com>
2026-06-30 10:58:37 +0200

    feat: enhance login header with dynamic rendering based on routing

--- src/app/shared/components/ui/login/login-header/login-header.ts
+++ src/app/shared/components/ui/login/login-header/login-header.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import {  Router, RouterLink } from "@angular/router";
+inject
 
 @Component({
   selector: 'app-login-header',
@@ -7,4 +8,10 @@ import { RouterLink } from "@angular/router";
   templateUrl: './login-header.html',
   styleUrl: './login-header.css',
 })
-export class LoginHeader {}
+export class LoginHeader {
+  private router = inject(Router);
+
+  get isLogingRoot(): boolean{
+    return this.router.url === '/login'
+  }
+}
