61bb4c95721b7586ee7f53e4579580fe452e253b
Maik G <maik.g@outlook.com>
2026-06-30 10:46:02 +0200

    feat: add sign-in component with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './shared/components/feature/layout-component';
 import { LoginComponent } from './shared/components/feature/login-component/login-component';
 import { Login } from './shared/components/ui/login/login/login';
 import { ForgotPassword } from './shared/components/ui/login/forgot-password/forgot-password';
+import { SignIn } from './shared/components/ui/login/sign-in/sign-in';
 import path from 'path';
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent, children: [
         {path: '', component: Login},
         {path: 'forgot-password', component: ForgotPassword},
+        {path: 'sign-in', component: SignIn},
     ]},
     { path: '**', redirectTo: '' },
 ];
--- src/app/shared/components/ui/login/login-header/login-header.ts
+++ src/app/shared/components/ui/login/login-header/login-header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-header.html',
   styleUrl: './login-header.css',
 })
