ff4de56fb5547d373e5bbea3edf61c70853dbafc
Maik G <maik.g@outlook.com>
2026-06-30 09:58:37 +0200

    feat: restructure login component with new routing and UI updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/feature/layout-component';
 import { LoginComponent } from './shared/components/feature/login-component/login-component';
+import { Login } from './shared/components/ui/login/login/login';
+import path from 'path';
 
 export const routes: Routes = [
     { path: '', component: LayoutComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', component: LoginComponent, children: [
+        {path: '', component: Login},
+    ]},
     { path: '**', redirectTo: '' },
 ];
--- src/app/shared/components/feature/login-component/login-component.ts
+++ src/app/shared/components/feature/login-component/login-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { LoginHeader } from '../../ui/login-header/login-header';
-import { LoginFooter } from '../../ui/login-footer/login-footer';
+import { LoginHeader } from '../../ui/login/login-header/login-header';
+import { LoginFooter } from '../../ui/login/login-footer/login-footer';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [LoginHeader, LoginFooter],
+  imports: [LoginHeader, LoginFooter, RouterOutlet],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
