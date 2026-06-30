452d5b8d2884dff35b5ef9e174ee5998b57a2858
Maik G <maik.g@outlook.com>
2026-06-30 10:17:05 +0200

    feat: add forgot password component with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/feature/layout-component';
 import { LoginComponent } from './shared/components/feature/login-component/login-component';
 import { Login } from './shared/components/ui/login/login/login';
+import { ForgotPassword } from './shared/components/ui/login/forgot-password/forgot-password';
 import path from 'path';
 
 export const routes: Routes = [
     { path: '', component: LayoutComponent },
     { path: 'login', component: LoginComponent, children: [
         {path: '', component: Login},
+        {path: 'forgot-password', component: ForgotPassword},
     ]},
     { path: '**', redirectTo: '' },
 ];
