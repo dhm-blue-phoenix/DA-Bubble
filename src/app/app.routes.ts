c05f8a23d58f56b950d1b10e3c4c387c408a582c
Maik G <maik.g@outlook.com>
2026-07-01 09:24:52 +0200

    feat: add select avatar component with routing and UI updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './shared/components/feature/login-component/logi
 import { Login } from './shared/components/ui/login/login/login';
 import { ForgotPassword } from './shared/components/ui/login/forgot-password/forgot-password';
 import { SignIn } from './shared/components/ui/login/sign-in/sign-in';
-import { ResetPassword } from'./shared/components/ui/login/reset-password/reset-password'
+import { ResetPassword } from'./shared/components/ui/login/reset-password/reset-password';
+import { SelectAvatar } from'./shared/components/ui/login/select-avatar/select-avatar';
 import path from 'path';
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent, children: [
         {path: '', component: Login},
         {path: 'sign-in', component: SignIn},
+        {path: 'select-avatar', component: SelectAvatar},
         {path: 'forgot-password', component: ForgotPassword},
         {path: 'reset-password', component: ResetPassword},
     ]},
---
+++ src/app/shared/components/ui/login/select-avatar/select-avatar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-select-avatar',
+  imports: [RouterLink],
+  templateUrl: './select-avatar.html',
+  styleUrl: './select-avatar.css',
+})
+export class SelectAvatar {}
