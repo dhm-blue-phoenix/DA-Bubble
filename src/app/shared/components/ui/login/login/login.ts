cb97cd072cdd13e5d14636c4b8082e4d4c0993f2
Maik G <maik.g@outlook.com>
2026-07-01 11:23:50 +0200

    feat: implement login functionality with email and password binding

--- src/app/shared/components/ui/login/login/login.ts
+++ src/app/shared/components/ui/login/login/login.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Database } from '../../../../services/db';
+import { FormsModule, NgModel } from '@angular/forms';
+import { environment } from '../../../../../../environment/environment';
+
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  db = inject(Database)
+
+  login_Data = {
+    email: '',
+    password: ''
+  }
+
+  guest_Data = {
+    email: environment.guest_email,
+    password: environment.guest_password
+  }
+
+test(){
+  console.log('email: ' + this.login_Data.email,'password: ' + this.login_Data.password)
+  this.db.login(this.login_Data.email, this.login_Data.password)
+}
+testguest(){
+  console.log('email: ' + this.guest_Data.email,'password: ' + this.guest_Data.password)
+}
+}
