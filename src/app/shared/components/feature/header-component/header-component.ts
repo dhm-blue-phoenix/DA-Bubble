3b5d84ed671f6ca6c206d26f85951dbde35abefc
Maik G <maik.g@outlook.com>
2026-07-08 19:00:22 +0200

    update: enhance profile dialog functionality and user status display in header component

--- src/app/shared/components/feature/header-component/header-component.ts
+++ src/app/shared/components/feature/header-component/header-component.ts
@@ -1,21 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProcessedData } from '../../../services/processed_data';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
 
   dialog_open = false
+  profile_open = false
   router = inject(Router)
   user = inject(ProcessedData)
 
   toggle_Dialog() {
     this.dialog_open = ! this.dialog_open
+  }
+  openProfile(){
+    this.profile_open = !this.profile_open
+    this.dialog_open = false
   }
 
   ngOnInit() {
