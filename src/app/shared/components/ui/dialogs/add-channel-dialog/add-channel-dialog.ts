e0ceefb3b337a87b68f673eb614d7319ee692b1d
Maik G <maik.g@outlook.com>
2026-07-20 11:09:03 +0200

    feat: enhance add channel dialog with form binding and input validation

--- src/app/shared/components/ui/dialogs/add-channel-dialog/add-channel-dialog.ts
+++ src/app/shared/components/ui/dialogs/add-channel-dialog/add-channel-dialog.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Database } from '../../../../services/db';
 
 @Component({
   selector: 'app-add-channel-dialog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-channel-dialog.html',
   styleUrl: './add-channel-dialog.css',
 })
-export class AddChannelDialog {}
+export class AddChannelDialog {
+
+  channel_name = ""
+  channel_descritpion = ""
+
+  db = inject(Database)
+
+  add_new_Channel (){
+    this.db.newChannel(this.db.getCurrentUserId(), this.channel_name, this.channel_descritpion)
+  }
+}
