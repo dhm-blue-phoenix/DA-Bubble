e33f3db59fd7692ee329e5724156085d88cd6348
Maik G <maik.g@outlook.com>
2026-07-29 10:26:04 +0200

    Refactor: Chat Ui

--- src/app/shared/components/ui/chat/chat.ts
+++ src/app/shared/components/ui/chat/chat.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, Output, EventEmitter } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Message } from '../../../interfaces/messages'
@@ -14,6 +14,8 @@ import { Database } from '../../../services/db';
 export class Chat {
   @Input() message!: Message
   @Input() sender?: Profile
+
+  @Output() openThread = new EventEmitter<string>()
 
   db = inject(Database)
 
