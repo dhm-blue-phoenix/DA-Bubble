bd04cc2b6a100e379569df3d53091e33a0b4cebb
Maik G <maik.g@outlook.com>
2026-07-29 17:07:35 +0200

    Feat: implement thread component with message display and separator styling

--- src/app/shared/components/ui/thread/thread.ts
+++ src/app/shared/components/ui/thread/thread.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Chat } from '../chat/chat';
+import { Message } from '../../../interfaces/messages';
+import { Profile } from '../../../interfaces/profile';
 
 @Component({
   selector: 'app-thread',
-  imports: [],
+  imports: [Chat],
   templateUrl: './thread.html',
   styleUrl: './thread.css',
 })
-export class Thread {}
+export class Thread {
+  @Input() rootMessage: Message | null = null
+  @Input() rootSender?: Profile
+  @Input() messages: { message: Message; sender?: Profile }[] = []
+}
