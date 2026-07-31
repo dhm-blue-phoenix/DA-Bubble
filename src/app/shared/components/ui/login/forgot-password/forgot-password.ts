3e9bea8925378f3ec23997e5e20b7a8522580259
Maik G <maik.g@outlook.com>
2026-07-31 11:35:02 +0200

    Feat: inject Database service and refactor sending_mail method for email handling

--- src/app/shared/components/ui/login/forgot-password/forgot-password.ts
+++ src/app/shared/components/ui/login/forgot-password/forgot-password.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
 import { UserFeedback } from '../user-feedback/user-feedback';
+import { Database } from '../../../../services/db';
 @Component({
   selector: 'app-forgot-password',
   imports: [RouterLink, FormsModule, UserFeedback],
@@ -10,13 +11,21 @@ import { UserFeedback } from '../user-feedback/user-feedback';
 })
 export class ForgotPassword {
 
+  db = inject(Database)
   send_email= ''
   show_feedback = signal(false);
+  
 
-  sending_mail(){
-    console.log(this.send_email)
+async sending_mail(){
+  try{
     this.show_feedback.set(true);
+    await this.db.sendEmailForPasswordReset(this.send_email)
+    this.send_email= ''
     setTimeout(() => this.show_feedback.set(false), 3000);
+  }
+  catch{
+    console.warn("email not sending at: " + this.send_email)
+  }
 
   }
 }
