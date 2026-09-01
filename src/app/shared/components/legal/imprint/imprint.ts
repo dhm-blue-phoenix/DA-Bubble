f1f7ffd3208b7f6d0fdcd804e641f71992616822
dhm-blue-phoenix <blue.dhm.dev@gmail.com>
2026-09-01 21:46:36 +0200

    add: impressum but without text

--- src/app/shared/components/legal/imprint/imprint.ts
+++ src/app/shared/components/legal/imprint/imprint.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoginHeader } from '../../ui/login/login-header/login-header';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [LoginHeader],
   templateUrl: './imprint.html',
   styleUrl: './imprint.css',
 })
 export class Imprint {
+  private router: Router = inject(Router);
 
+  public navigationBackLogin(): void {
+    this.router.navigate(['/']);
+  };
 }
